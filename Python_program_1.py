1021227fbd14636bb18d1bfa18977fe7a710c5fa
Saheen AHZAN <saheen619.klm@gmail.com>
2022-10-12 22:50:27 +0300

    Create Python_program_1.py

---
+++ Python_program_1.py
@@ -0,0 +1,55 @@
+#my first python program
+
+a= 10
+b = 9.2
+c = 'saheen'
+d = a + b
+e = True
+f = a+10
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+# =============== type(a)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print("The Value of f is = ",f , "PRINTED!!")
+
+# ======== TYPE CASTING -> How to convert one datatype to another
+
+var1 = 123
+var2 = "123"
+print("value of var1 :",var1)
+print("value of var2 :",var2)
+print("Datatype of var1 is ",type(var1))
+print("Datatype of var2 is ",type(var2))
+
+
+var3 = int(var2)
+print("Datatype of var3 is ",type(var3))
+print("var2 + 10 =",var3+10)
+
+
+var4 = 19.4
+var5 = int(var4)
+print("value of var4 :",var4)
+print("Datatype of var4 is ",type(var4))
+print("Datatype of var5 is ",type(var5))
+print("var4 + 10 =",var4+10)
+print("var5 + 10 =",var5+10)
+
+var6 = 84
+var7 = float(var6)
+print("value of var6 :",var6)
+print("Datatype of var6 is ",type(var6))
+print("Datatype of var7 is ",type(var7))
+print("var6 + 10 =",var6+10)
+print("var7 + 10 =",var7+10)
